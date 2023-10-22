920afba3b474f3e118a75df15e4f4ffe1c89252e
leok-20 <32753695+leok-20@users.noreply.github.com>
2023-10-21 20:35:25 -0700

    added basic homepage & patched header/footer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Cartify
 Ecommerce Application
+
+## Tech Stack
+- Backend: Django
+- Frontend: Angular
+
+## Libraries
+- https://richnologies.gitbook.io/ngx-stripe/core-concepts/checkout
+- https://getbootstrap.com/
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxStripeModule } from 'ngx-stripe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { NgIconsModule } from '@ng-icons/core';
+import { featherSearch, featherUser, featherShoppingCart } from '@ng-icons/feather-icons';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './utilities/navbar/navbar.component';
+import { FooterComponent } from './utilities/footer/footer.component';
+import { CheckoutComponent } from './utilities/checkout/checkout.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CheckoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgxStripeModule.forRoot('***your-stripe-publishable-key***'),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    NgIconsModule.withIcons({ featherSearch, featherUser, featherShoppingCart })
   ],
   providers: [],
   bootstrap: [AppComponent]
