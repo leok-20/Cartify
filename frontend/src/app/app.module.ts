c3d8f3fd7002ddbd68c56725f0327e81b6bf5121
leok-20 <32753695+leok-20@users.noreply.github.com>
2023-10-21 22:53:14 -0700

    themes & more pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { NgIconsModule } from '@ng-icons/core';
-import { featherSearch, featherUser, featherShoppingCart } from '@ng-icons/feather-icons';
+import { featherSearch, featherUser, featherShoppingCart, featherSun, featherMoon } from '@ng-icons/feather-icons';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './utilities/navbar/navbar.component';
@@ -15,9 +15,17 @@ import { FooterComponent } from './utilities/footer/footer.component';
 import { CheckoutComponent } from './utilities/checkout/checkout.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GroceryComponent } from './pages/grocery/grocery.component';
+import { StoresComponent } from './pages/stores/stores.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', component: HomeComponent },
+  { path: 'grocery', component: GroceryComponent },
+  { path: 'stores', component: StoresComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
 ]
 
 @NgModule({
@@ -26,11 +34,16 @@ const routes: Routes = [
     NavbarComponent,
     FooterComponent,
     CheckoutComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    GroceryComponent,
+    StoresComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
     NgbModule,
     NgxStripeModule.forRoot('***your-stripe-publishable-key***'),
     CommonModule,
@@ -38,7 +51,7 @@ const routes: Routes = [
     ReactiveFormsModule,
     RouterModule,
     HttpClientModule,
-    NgIconsModule.withIcons({ featherSearch, featherUser, featherShoppingCart })
+    NgIconsModule.withIcons({ featherSearch, featherUser, featherShoppingCart, featherSun, featherMoon })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/utilities/navbar/navbar.component.ts
+++ frontend/src/app/utilities/navbar/navbar.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  darkmode: boolean = false
 
+  switchTheme() {
+    this.darkmode = !this.darkmode
+    if (this.darkmode) {
+      document.querySelector('html')?.setAttribute('data-bs-theme', 'dark');
+    } else {
+      document.querySelector('html')?.setAttribute('data-bs-theme', 'light');
+    }
+  }
 }
