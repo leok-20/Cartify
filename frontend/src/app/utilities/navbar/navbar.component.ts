0962190916460f0929782502956067a2affba9ad
leok-20 <32753695+leok-20@users.noreply.github.com>
2023-10-22 19:20:08 -0700

    about page & patched cart

--- frontend/src/app/utilities/navbar/navbar.component.ts
+++ frontend/src/app/utilities/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CartItem } from 'src/app/models/models';
 
 @Component({
   selector: 'navbar',
@@ -9,6 +10,13 @@ export class NavbarComponent {
   @Output() themePicked = new EventEmitter<any>();
   darkmode: boolean = false
 
+  cartList: CartItem[] = [
+    { id: 1, name: 'Milk', amount: 1, price: 3.07 },
+    { id: 2, name: 'Apples', amount: 4, price: 1.99 },
+    { id: 3, name: 'Bread', amount: 2, price: 3.29 },
+    { id: 4, name: 'Sushi', amount: 1, price: 12.55 }
+  ]
+
   switchTheme() {
     this.darkmode = !this.darkmode
     if (this.darkmode) {
@@ -17,5 +25,10 @@ export class NavbarComponent {
       document.querySelector('html')?.setAttribute('data-bs-theme', 'light');
     }
     this.themePicked.emit(this.darkmode)
+  }
+
+  deleteItem(id: number) {
+    console.log(this.cartList.findIndex((item: CartItem) => { return item.id == id }))
+    this.cartList.splice(this.cartList.findIndex((item: CartItem) => { return item.id == id }), 1)
   }
 }
